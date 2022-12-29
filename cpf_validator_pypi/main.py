b45f48c9f1b9ee74934b9e802843180ac8b5da33
Giovani José de Souza <brunosouza.cup@gmail.com>
2022-12-29 00:04:31 -0300

    update

--- cpf_validator_pypi/main.py
+++ cpf_validator_pypi/main.py
@@ -1,12 +1,10 @@
+# from datetime import datetime, timedelta
+# from datas_br import DatasBr
+from acesso_cep import BuscaEndereco
 
-from cellBr import TelefonesBr
-import re
+cep = 25870145
+objeto_cep = BuscaEndereco(cep)
+print(objeto_cep)
 
-telefone = "552126481234"
-telefone_objeto = TelefonesBr(telefone)
 
-#padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
-#resposta = re.search(padrao,telefone)
-#print(resposta.group())
 
-print(telefone_objeto)
